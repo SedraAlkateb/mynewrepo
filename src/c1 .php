c6dd5416f4be43a3cd2da273addac705d4d74574
sedra_alkateb <sedraalkateb3@gmail.com>
2022-08-08 13:16:16 +0300

    initial

---
+++ src/c1 .php
@@ -0,0 +1,46 @@
+<?php
+$A1 = array ("red","green","blue");
+    array ("kn");
+    if($d = 1){
+ 
+for($i=0;$i<8;$i = $i+2 ){
+   
+print $cloors;
+   $F= "SFS" ;
+ array ("kn")print $_REQUEST['name'];
+  if($fd > 5)
+   {
+     
+print $_REQUEST['name'];
+   print "d";
+   if($fd)
+   {
+   
+print "d";
+   }
+   }
+ }}
+ ?><?php
+$A1 = array ("red","green","blue");
+    array ("kn");
+    if($d = 1){
+ 
+for($i=0;$i<8;$i = $i+2 ){
+   
+print $cloors;
+   $F= "SFS" ;
+ array ("kn")print $_REQUEST['name'];
+  if($fd > 5)
+   {
+     
+print $_REQUEST['name'];
+   print "d";
+   if($fd)
+   {
+   
+print "d";
+   }
+   }
+ }}
+ ?><?php
+?>
